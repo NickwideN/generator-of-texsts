cc7ee07d32e794b2cbdeffd08c548f45b3785439
NickwideN <37487668+NickwideN@users.noreply.github.com>
2018-03-26 04:30:29 +0300

    Update train.py
    Теперь пары слов сохраняются как список Lib, в конце выводится в файл model.txt

--- train.py
+++ train.py
@@ -1,58 +1,39 @@
 import re
 
-#очистим файл model.txt
-model = open('model.txt', 'w')  
-model.close()
-
-#ну, приступим
 train_text = open('text.txt', 'r', encoding="utf-8")
-#объявление переменной, содержащая последнее слово предыдущей строчки
-last_word = ''
+last_word = ''  #содержит последнее слово предыдущей строчки
+Lib = []        #библиотека пар слов
 for line in train_text:
-    # вставим в начало строчки последнее слово предыдущей
-    # строчки
+    # вставим в начало строчки последнее слово предыдущей строчки
     line = last_word + ' ' + line 
     # приведем все символы к lowercase
     line = line.lower()
     # оставим только алфавитные символы
-    line = re.sub('[^a-zа-я ']', '', line)
-    #line = OnlyAlpha(line)
-   
+    line = re.sub('[^a-zа-я ]', '', line)
     # разделение строки по словам
     line_list = line.split()
+    #зафиксируем последнее слово строчки
     if line_list:
         last_word = line_list[-1]
-    
-    
-    #работа с парами слов
+    #ввод пар строк в библиотеку
     for i in range(len(line_list) - 1):
         new_pair = line_list[i] + '\t' +  line_list[i + 1]
-        #print('\n\nnext pair: ', new_pair)
-        model = open('model.txt', 'r+')
-        model_line = ' '
-        is_new_pair_was = False
-        while model_line:
-            model_line = model.readline()
-            #print('курсор на: ', model.tell(), ' ', model_line)
-            pair_model = model_line[:model_line.find(' ')]
-            #print('pair_model', pair_model)
-            if pair_model == new_pair:
-                model.seek(model.tell() - 2)
-                number = str(int(model.read(1)) + 1)
-                model.seek(model.tell() - 1)
-                model.write(number)
-                is_new_pair_was = True
-                #print('!!break курсор: ', model.tell()) 
-                #print(pair_model, '==', new_pair)
+        new_pair_is_in_Lib = False
+        for row in Lib:
+            if row[0] == new_pair:
+                row[1] += 1 
+                new_pair_is_in_Lib = True
                 break
-        if (not(is_new_pair_was)):    
-            model.write(new_pair + ' ' + '1' + '\n')
-        model.close()
-    #print(line_list)
-    
-    
-print('ok')
+        if new_pair_is_in_Lib == False:
+            Lib.append([new_pair, 1])
+#вывод быблиотеки в файл model.txt
+model = open('model.txt', 'w')
+for row in Lib:
+    model.write(row[0] + ' ' + str(row[1]) + '\n')
+model.close()
+ 
 train_text.close()
+print('ok')
 
 
 
