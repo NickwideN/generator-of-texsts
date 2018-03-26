4ccd913d48aa98be1afa5038dccc64b61418c507
NickwideN <37487668+NickwideN@users.noreply.github.com>
2018-03-26 06:01:42 +0300

    Update generate.py
    В память импортируется вся библиотека пар слов.
    Добавлена конструкция if __name__ == '__main__':

--- generate.py
+++ generate.py
@@ -1,41 +1,44 @@
 import numpy as np
-def next_word(word_1):
-    model = open('model.txt', 'r')
-    #создаем массив из возможных слов-2
-    words_2 = []
-    words_2_truffic = []
-    line_model = 'string'
-    while line_model and line_model not in '  ':
-        line_model = model.readline()
-        if not(line_model): break
-        line_model_arr = line_model.split()
-        if line_model_arr[0] == word_1:
-            words_2.append(line_model_arr[1])
-            words_2_truffic.append(int(line_model_arr[2]))
-    sum_traffic = sum(words_2_truffic)
-    for i in range(len(words_2_truffic)):
-        words_2_truffic[i] /= sum_traffic
-        
-    #print('words_2', words_2)
-    #print('words_2_truffic', words_2_truffic)
-    #print(sum(words_2_truffic))
-    #создали массивы
-    if words_2:
-        new_word = np.random.choice(words_2, 1, p=words_2_truffic)[0]
-    else:
-        print("\nI don't know what '", word_1,"' mean", sep = '')
-        exit(1)
-    
-    model.close()
-    return new_word
 
-print('input the first word')
-word = input()
-print('input number of next words')
-size = int(input())
-print(word, end = ' ')
-for i in range(size):
-    word = next_word(word)
-    print(word, end = ' ') 
-print('\n')
+def next_word(word_1, Lib):
+	#создаем массив из возможных слов-2
+	words_2 = []
+	words_2_truffic = [] #количество встреч соответствующей пары
+	line_model = 'string'
+	for row in Lib:
+	    if row:
+    		if row[0] == word_1:
+    			words_2.append(row[1])
+    			words_2_truffic.append(int(row[2]))
+	sum_traffic = sum(words_2_truffic)
+	for i in range(len(words_2_truffic)):
+		words_2_truffic[i] /= sum_traffic
 
+	#создали массивы
+	if words_2:
+		new_word = np.random.choice(words_2, 1, p=words_2_truffic)[0]
+	else:
+		print("\nI don't know what '", word_1, "' mean", sep='')
+		exit(1)
+	return new_word
+
+
+if __name__ == '__main__':
+    #импорт библиотеки
+	model = open('model.txt', 'r', encoding='utf-8')
+	Lib = model.read().split('\n')
+	for i in range(len(Lib)):
+	    Lib[i] = Lib[i].split()
+	#ввод первого слова
+	print('input the first word')
+	word = input()
+	# ввод количества последующих слов
+	print('input number of next words')
+	size = int(input())
+	#вывод последовательности
+	print(word, end=' ')
+	for i in range(size):
+		word = next_word(word, Lib)
+		print(word, end=' ')
+	print('\n')
+	model.close()
