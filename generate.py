202c29e271da19e259a3bc90ec810725749c6f55
NickwideN <37487668+NickwideN@users.noreply.github.com>
2018-03-31 18:13:55 +0300

    Lib теперь dict

--- generate.py
+++ generate.py
@@ -1,41 +1,31 @@
 import numpy as np
-
+import ast
 def next_word(word_1, Lib):
-	#создаем массив из возможных слов-2
-	words_2 = []
-	words_2_truffic = [] #количество встреч соответствующей пары
-	line_model = 'string'
-	for row in Lib:
-	    if row:
-    		if row[0] == word_1:
-    			words_2.append(row[1])
-    			words_2_truffic.append(int(row[2]))
-	sum_traffic = sum(words_2_truffic)
-	for i in range(len(words_2_truffic)):
-		words_2_truffic[i] /= sum_traffic
-
-	#создали массивы
-	if words_2:
-		new_word = np.random.choice(words_2, 1, p=words_2_truffic)[0]
-	else:
-		print("\nI don't know what '", word_1, "' mean", sep='')
-		exit(1)
-	return new_word
+    if word_1 in Lib:
+        words_2_dict = Lib[word_1]
+        words_2 = list(words_2_dict.keys())
+        words_2_traffic = list(words_2_dict.values()) #количество встреч соответствующей пары
+        sum_traffic = sum(words_2_dict.values())
+        for i in range(len(words_2_traffic)):
+            words_2_traffic[i] /= sum_traffic
+        new_word = np.random.choice(words_2, 1, p=words_2_traffic)[0]
+    else:
+        print("\nI don't know what '", word_1, "' mean", sep='')
+        exit(1)
+    return new_word
 
 
 if __name__ == '__main__':
     #импорт библиотеки
 	model = open('model.txt', 'r', encoding='utf-8')
-	Lib = model.read().split('\n')
-	for i in range(len(Lib)):
-	    Lib[i] = Lib[i].split()
+	Lib = ast.literal_eval(model.read())
 	#ввод первого слова
 	print('input the first word')
 	word = input()
 	# ввод количества последующих слов
 	print('input number of next words')
 	size = int(input())
-	#вывод последовательности
+	# вывод последовательности
 	print(word, end=' ')
 	for i in range(size):
 		word = next_word(word, Lib)
