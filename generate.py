a2fa954479f8b3eb1a9401cec1acb69f6218597c
NickwideN <37487668+NickwideN@users.noreply.github.com>
2018-04-01 19:10:36 +0300

    Добавлен консольный интерфейс

--- generate.py
+++ generate.py
@@ -1,5 +1,20 @@
 import numpy as np
 import ast
+import argparse
+import sys
+import random
+parser = argparse.ArgumentParser(description='Ganerate sequence of words, based on model, which created by train.txt')
+
+parser.add_argument('--model','-m', type=argparse.FileType('r'), default=open('model.txt', 'r'), help='Way to file for reading model')
+
+parser.add_argument('--seed', '-s', default=None, help='The first word of the sequence. (default: random word')
+
+parser.add_argument('--length', '-l', type=int, help='Length of the sequence. (default: random(max = 100))', default=None)
+
+parser.add_argument('--output', '-o', help='Way to write the sequence', default=sys.stdout)
+args = parser.parse_args()
+
+
 def next_word(word_1, Lib):
     if word_1 in Lib:
         words_2_dict = Lib[word_1]
@@ -15,20 +30,23 @@ def next_word(word_1, Lib):
     return new_word
 
 
-if __name__ == '__main__':
+def main():
     #импорт библиотеки
-	model = open('model.txt', 'r', encoding='utf-8')
+	model = args.model
 	Lib = ast.literal_eval(model.read())
 	#ввод первого слова
-	print('input the first word')
-	word = input()
+	word = args.seed
+	if not(word): word = random.choice(list(Lib.keys()))
 	# ввод количества последующих слов
-	print('input number of next words')
-	size = int(input())
+	size = args.length
+	if not(size): size = random.randint(1, 100)
 	# вывод последовательности
-	print(word, end=' ')
+	args.output.write(word + ' ')
 	for i in range(size):
 		word = next_word(word, Lib)
-		print(word, end=' ')
-	print('\n')
-	model.close()
+		args.output.write(word + ' ')
+	args.output.write('\n')
+    
+    
+if __name__ == '__main__':
+    main()
