aa8b4595f70bc43cbb5b410520ea9b4d35ff079b
NickwideN <37487668+NickwideN@users.noreply.github.com>
2018-04-16 14:39:51 +0300

    defaultdict   issue #6

--- train.py
+++ train.py
@@ -3,9 +3,10 @@ import argparse
 
 import os
 import ast
+from collections import defaultdict
 
 parser = argparse.ArgumentParser( formatter_class=argparse.ArgumentDefaultsHelpFormatter, description='Creat a library for generate of sequence of words. After train.py use generate.py')
-parser.add_argument('--input-dir', '-i', default='stdin', dest='input_dir', help='Way to collection of train_files, only .txt files exept model')
+parser.add_argument('--input-dir', '-i', default='stdin', dest='input_dir', help='Way to collection of train_files, only .txt files exept model.txt')
 
 parser.add_argument('--model', '-m', type=argparse.FileType('w'), default=open('model.txt', 'w'), help='Way to file for saving model')
 
@@ -33,18 +34,15 @@ def add_line_in_Lib( line, Lib, last_word = ''):
         last_word = line_list[-1]
     #ввод пар строк в библиотеку
     for i in range(len(line_list) - 1):
-        if line_list[i] in Lib:
-            if line_list[i + 1] in Lib[line_list[i]]:
-                Lib[line_list[i]][line_list[i + 1]] += 1
-            else:
-                Lib[line_list[i]][line_list[i + 1]] = 1
+        if line_list[i + 1] in Lib[line_list[i]]:
+            Lib[line_list[i]][line_list[i + 1]] += 1
         else:
-            Lib[line_list[i]] = {line_list[i + 1]: 1}
+            Lib[line_list[i]][line_list[i + 1]] = 1
     return Lib, last_word
 
 def main():    
+    Lib = defaultdict(dict)       #библиотека пар слов
     if args.input_dir != 'stdin':
-        Lib = {}       #библиотека пар слов
         pattern = re.compile(r'.*\.txt')
         exept_pattern = re.compile(args.model.name)
         train_list = [_file for _file in os.listdir(args.input_dir) if pattern.match(_file) and not(exept_pattern.match(_file))]
@@ -52,19 +50,17 @@ def main():
             train_text = open(args.input_dir + _file, 'r', encoding="utf-8")
             Lib = add_file_in_Lib(train_text, Lib)
             train_text.close()
-        args.model.write(str(Lib))
-        print('ok')
     else:
-        Lib = {}
         train_text = ''
         while True:
             line = input()
             if not(line):
                 break
-            train_text += '\n' + line
+            train_text += ' ' + line
+        print("train_text:  ", train_text)
         Lib = add_line_in_Lib(train_text, Lib)[0]
-        args.model.write(str(Lib))
-        print('ok')
+    args.model.write(str(dict(Lib)))
+    print('ok')
 
 
 if __name__ == '__main__':
